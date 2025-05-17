98319c0f2dc5869e95acd08dc3fe7ef6e6b2d098
Clarisse-X <clarissexk@gmail.com>
2025-05-17 13:42:28 +0700

    Delete storage directory

--- storage/framework/views/1493f1f05b428822e026d4e3cfe3b185.php
+++
@@ -1,9 +0,0 @@
-<?php $__env->startSection('content'); ?>
-<div class="container mx-auto px-4 py-8">
-    <h1 class="text-3xl font-bold mb-6">Checkout</h1>
-    <p>This is a placeholder for your checkout process. Integrate payment gateway here.</p>
-    <a href="<?php echo e(route('dashboard')); ?>" class="mt-4 inline-block text-blue-600 hover:underline">← Back to Home</a>
-</div>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\xampp\htdocs\CLARISSE\FinalProject_Backend_BNCC\resources\views/cart/checkout.blade.php ENDPATH**/ ?>
